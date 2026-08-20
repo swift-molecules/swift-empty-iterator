6ae2ce81edce993d83e4857fa336bec366322af9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:06:50 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,25 +1,31 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-empty-iterator-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Empty Iterator Primitives",
             targets: ["Empty Iterator Primitives"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-empty-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-iterator-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-empty-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-iterator-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
